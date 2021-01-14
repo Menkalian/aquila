13885ddda3250dadad2da0c310821c8e486b2800
Kilian Krampf <kiliankra@gmail.com>
2021-01-15 00:03:27 +0100

    aquila-config added

---
+++ aquila-config/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "de.menkalian.aquila"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
