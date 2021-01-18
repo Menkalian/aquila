d6f16e15f9222b1b7195fb78082f44977a90dc0b
Kilian Krampf <kiliankra@gmail.com>
2021-01-18 20:32:39 +0100

    Basic configuration

--- aquila-server/src/main/kotlin/de/menkalian/aquila/server/Application.kt
+++ aquila-server/src/main/kotlin/de/menkalian/aquila/server/Application.kt
@@ -1,9 +1,21 @@
 package de.menkalian.aquila.server
 
 import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.HttpsRedirect
+import io.ktor.routing.routing
+import io.ktor.serialization.json
 
-fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args)
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
+    install(HttpsRedirect) {
+        sslPort = 8083
+        permanentRedirect = false
+    }
+    install(ContentNegotiation) {
+        json()
+    }
 
 }
