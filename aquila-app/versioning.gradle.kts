c05e2c2a1349272c08e587fd7596ae08c75051e9
Kilian Krampf <kiliankra@gmail.com>
2021-01-18 20:20:13 +0100

    Adapt versioning

--- aquila-app/versioning.gradle.kts
+++ aquila-app/versioning.gradle.kts
@@ -9,17 +9,20 @@ fun autoIncrementBuildNumber() {
         versionProps.load(versionPropsFile.inputStream())
         extra["buildNumber"] = versionProps.getProperty("VERSION_BUILD", "0").toInt() + 1
         versionProps["VERSION_BUILD"] = extra["buildNumber"].toString()
-        versionProps.store(versionPropsFile.outputStream(), "Automatic Build Number. DO NOT ALTER!!!")
+        versionProps.store(
+            versionPropsFile.outputStream(),
+            "Automatic Build Number. DO NOT ALTER!!!"
+        )
     }
-}
-
-gradle.taskGraph.whenReady {
-    if (this.hasTask(tasks.getByName("assembleRelease")))
-        autoIncrementBuildNumber()
 }
 
 if (versionPropsFile.canRead()) {
     val versionProps = java.util.Properties()
     versionProps.load(versionPropsFile.inputStream())
-    extra["buildNumber"] = versionProps.getProperty("VERSION_BUILD", "0").toInt()
+    extra["buildNumber"] = versionProps.getProperty("VERSION_BUILD", "0").toInt() + 1
+}
+
+gradle.taskGraph.whenReady {
+    if (this.hasTask(tasks.getByName("assembleRelease")))
+        autoIncrementBuildNumber()
 }
