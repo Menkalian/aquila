aefef8fac6ef4c796500e0762d7faab5a0cb87ed
Kilian Krampf <kiliankra@gmail.com>
2021-01-24 14:54:55 +0100

    support translations

--- aquila-app/app-loader/src/main/java/de/menkalian/aquila/MainActivity.kt
+++ aquila-app/app-loader/src/main/java/de/menkalian/aquila/MainActivity.kt
@@ -5,14 +5,20 @@ import android.content.Intent
 import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
 import androidx.core.content.FileProvider
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     val installViewModel: InstallViewModel by viewModels()
+    lateinit var stringUpdateJob: Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +33,12 @@ class MainActivity : ComponentActivity() {
             if (installViewModel.isApiCompatible()) {
                 val updateApk = installViewModel.prepareUpdate()
                 AlertDialog.Builder(this@MainActivity)
-                    .setMessage("Update Bereit. Bitte bestätige die Installation und erlaube alles.")
-                    .setPositiveButton("Ok") { interf, id ->
+                    .setMessage(R.string.aquila_update_ready)
+                    .setPositiveButton(getString(R.string.aquila_update_confirm)) { _, _ ->
                         val installIntent = Intent(Intent.ACTION_VIEW)
                         installIntent.setDataAndType(
                             FileProvider.getUriForFile(
-                                getApplication(),
+                                application,
                                 "${application.applicationContext.packageName}.update.provider",
                                 updateApk
                             ),
@@ -41,16 +47,34 @@ class MainActivity : ComponentActivity() {
                         installIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                         installIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
                         application.startActivity(installIntent)
-                    }.setNegativeButton("Abbrechen") { interf, id ->
+                    }.setNegativeButton(R.string.aquila_update_cancel) { _, _ ->
                         this@MainActivity.finishAffinity()
                     }.show()
             } else {
                 AlertDialog.Builder(this@MainActivity)
                     .setMessage("API is incompatible\n Please download a newer version!")
-                    .setPositiveButton("Ok") { interf, id ->
+                    .setPositiveButton("Ok") { _, _ ->
                         this@MainActivity.finishAffinity()
+                    }.show()
+            }
+        }
+        stringUpdateJob = installViewModel.viewModelScope.launch {
+            val loadingText: TextView = findViewById(R.id.loading_text)
+            withContext(Dispatchers.Main) {
+                while (true) {
+                    loadingText.text = when (Random.nextInt(10)) {
+                        0 -> getString(R.string.aquila_loading_string_0)
+                        1 -> getString(R.string.aquila_loading_string_1)
+                        2 -> getString(R.string.aquila_loading_string_2)
+                        3 -> getString(R.string.aquila_loading_string_3)
+                        4 -> getString(R.string.aquila_loading_string_4)
+                        5 -> getString(R.string.aquila_loading_string_5)
+                        6 -> getString(R.string.aquila_loading_string_6)
+                        7 -> getString(R.string.aquila_loading_string_7)
+                        8 -> getString(R.string.aquila_loading_string_8)
+                        else -> getString(R.string.aquila_loading_string_9)
                     }
-                    .create().show()
+                }
             }
         }
     }
